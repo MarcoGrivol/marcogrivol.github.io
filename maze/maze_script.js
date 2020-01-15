9f28981315ac73b8ca602cef367452cb9c7121fd
MarcoGrviol <mbgrivol@gmail.com>
2020-01-15 19:10:20 -0300

    canvas and buttons update

--- maze/maze_script.js
+++ maze/maze_script.js
@@ -1,14 +1,15 @@
-var grid ,rows, cols;
-var w_size = 30;
+var grid;
+var rows; // y coordinates :
+var cols; // x coordinates --
+var w_size = 50;
 var onPressed = false;
+var pencil_on = true;
+var eraser_on = false;
 
 function setup() {
     var div = document.getElementById('maze_container');
-    var height = floor(div.offsetHeight);
-    var width = floor(div.offsetWidth);
-
-    var canvas_div = createCanvas(width, height);
-    canvas_div.parent('maze_container');
+    var height = div.offsetHeight;
+    var width = div.offsetWidth;
 
     // creates 2DArray as grid[rows][cols]
     rows = floor(height / w_size);
@@ -24,6 +25,11 @@ function setup() {
             grid[i][j] = new Cell(j * w_size, i * w_size, w_size);
         }
     }
+
+    // will center the canvas inside the div
+    var canvas_div = createCanvas(cols * w_size, rows * w_size);
+    canvas_div.parent('maze_container');
+    canvas_div.center();
 }
 
 function draw() {
@@ -38,9 +44,11 @@ function draw() {
         var x = floor(mouseY / w_size); // x is the row(height)
         var y = floor(mouseX / w_size); // y is the col(width)
         if (x >= 0 && x < rows && y >= 0 && y < cols) {
-            grid[x][y].fill = true;
+            if (pencil_on)
+                grid[x][y].fill = true;
+            else if (eraser_on)
+                grid[x][y].fill = false;
         }
-        console.log(x, y)
     }
 }
 
@@ -50,4 +58,14 @@ function mousePressed() {
 
 function mouseReleased() {
     onPressed = false;
+}
+
+function pencil() {
+    pencil_on = true;
+    eraser_on = false;
+}
+
+function eraser() {
+    eraser_on = true;
+    pencil_on = false;
 }

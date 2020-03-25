b445e44693cf7fc8a74d89bb6a056344acb15459
MarcoGrviol <mbgrivol@gmail.com>
2020-03-24 22:51:18 -0300

    ballons & wind, physics & animation

--- jsArt/bubbles.js
+++ jsArt/bubbles.js
@@ -2,15 +2,19 @@ var rainbow_color = ['#FF0000', '#FF7F00', '#FFFF00',
                      '#00FF00', '#0000FF', '#2E2B5F', '#8B00FF'];
 var index = 0;
 var bubbles = [];
-var interval = 5;
+var INTERVAL = 2;
+var interval_count = INTERVAL;
 var gravity_constant = 0.02;
-var create_wind = false;
 var point_a = point_b = [0, 0];
 var get_point_a = true;
 var winds = [];
+var canvas_width = 800;
+var canvas_height = 800;
+var resolution = 10;
+var grid = new windGrid(canvas_width, canvas_height, resolution);
 
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(canvas_width, canvas_height);
 }
 
 function draw() {
@@ -30,6 +34,12 @@ function draw() {
             i--; // the list will be 1 short, this accounts for the resizing
         } else {
             bubbles[i].draw();
+            // check if bubble is in wind
+            var [flag, index_i, index_j] = grid.checkWind(bubbles[i].getPos())
+            if (flag) {
+                var w = grid.windParameters(index_i, index_j);
+                bubbles[i].windEffect(w);
+            }
             bubbles[i].float();
         }
     }
@@ -37,19 +47,21 @@ function draw() {
     for (var i = 0; i < winds.length; i++) {
         winds[i].draw(); 
     }
+    grid.draw();
     timing();
 }
 
 function getWind() {
-    if (mouseIsPressed) {
+    if (mouseIsPressed && isMouseInCanvas(mouseX, mouseY)) {
         if (get_point_a) {
             point_a = [mouseX, mouseY];
             get_point_a = false;
         }
         point_b = [mouseX, mouseY];
-        if (euclidianDistance(point_a, point_b) > 10) {
+        if (euclidianDistance(point_a, point_b) > 5) {
             var w = new Wind(point_a, point_b);
             winds.push(w);
+            grid.setWind(mouseX, mouseY, point_a, point_b);
             resetPoints();
         }
     } else {
@@ -58,10 +70,12 @@ function getWind() {
 }
 
 function getBubble() {
-    if (mouseIsPressed && interval == 5) {
-        var c = color(rainbow_color[index]);
-        var b = new Bubble(mouseX, mouseY, c);
-        bubbles.push(b);
+    if (mouseIsPressed && isMouseInCanvas(mouseX, mouseY)) {
+        if (interval_count == INTERVAL) {
+            var c = color(rainbow_color[index]);
+            var b = new Bubble(mouseX, mouseY, c);
+            bubbles.push(b);
+        }
     }
 }
 
@@ -69,15 +83,15 @@ function getBubble() {
 function euclidianDistance(point_a, point_b) {
     var x = point_a[0] - point_b[0];
     var y = point_a[1] - point_b[1];
-    return Math.sqrt(x ** 2, y ** 2);
+    return Math.sqrt(x ** 2 + y ** 2);
 }
 
 // avoids drawing too many bubbles and overlapping
 function timing() {
-    if (interval == 0) {
-        interval = 5;
+    if (interval_count == 0) {
+        interval_count = INTERVAL;
     } else {
-        interval--;
+        interval_count--;
     }
     if (index == rainbow_color.length - 1) {
         index = 0;
@@ -89,4 +103,12 @@ function timing() {
 function resetPoints() {
     get_point_a = true;
     point_b = point_a;
+}
+
+function isMouseInCanvas(mouseX, mouseY) {
+    if (mouseX < canvas_width && mouseX > 0) {
+        if (mouseY < canvas_height && mouseY > 0) {
+            return true;
+        } return false;
+    } return false;
 }
--- jsArt/classes.js
+++ jsArt/classes.js
@@ -1,3 +1,4 @@
+// bubbles animation and physics
 class Bubble {
     constructor(x, y, color) {
         this.x = x;
@@ -7,8 +8,13 @@ class Bubble {
         this.gravity = 1;
     }
     
-    draw() {
-        fill(this.color);
+    draw(c=false) {
+        stroke(0);
+        if (c) {
+            fill(color(c));
+        } else { 
+            fill(this.color);
+        }
         ellipse(this.x, this.y, this.diameter);
     }
 
@@ -17,18 +23,76 @@ class Bubble {
         this.gravity += gravity_constant;
     }
 
+    windEffect(wind) {
+        var [heading, strength] = wind;
+        if (heading > 0 && heading < 90) {
+            // nordeste
+            var y_offset = heading / 90;
+            var x_offset = 1 - y_offset;
+            this.x += x_offset * strength;
+            this.y -= y_offset * strength;
+        }
+        else if (heading > 90 && heading < 180) {
+            // noroeste
+            var x_offset = heading / 180;
+            var y_offset = 1 - x_offset;
+            this.x -= x_offset * strength;
+            this.y -= y_offset * strength;
+        }
+        else if (heading > -180 && heading < -90) {
+            // sudoeste
+            var y_offset = heading / -180;
+            var x_offset = 1 - y_offset;
+            this.x -= x_offset * strength;
+            this.y += y_offset * strength;
+        }
+        else if (heading > -90 && heading < 0) {
+            // sudeste
+            var x_offset = heading / -90;
+            var y_offset = 1 - x_offset;
+            this.x += x_offset * strength;
+            this.y += y_offset * strength;
+        }
+        if (heading == 0 || heading == 180) {
+            // horizontal
+            if (heading == 0) {
+                var x_offset = 1;
+            } else {
+                var x_offset = -1;
+            }
+            this.x += x_offset * strength;
+        } 
+        else if (heading == 90 || heading == -90) {
+            // vertical
+            if (heading == 90) {
+                var y_offset = 1;
+            } else {
+                var y_offset = -1;
+            }
+            this.y += y_offset * strength;
+        }
+    }
+
     delete() {
-        if (this.y < -this.diameter) {
+        if (this.y < 0) {
             return true;
         } return false;
     }
+
+    getPos() {
+        return [this.x, this.y];
+    }
 }
 
+// only responsible for animation and wind visualization
+// does not affect the bubbles
 class Wind {
     constructor(point_a, point_b) {
         this.a = point_a;
         this.b = point_b;
         this.color = color('#4aeff7');
+        this.slope = this.lineSlope();
+        this.OFFSET_CONSTANT = 5;
     }
 
     draw() {
@@ -36,7 +100,108 @@ class Wind {
         // main line
         line(this.a[0], this.a[1], this.b[0], this.b[1]);
         // lines above and below
-        line(this.a[0] + 5, this.a[1] + 5, this.b[0] + 5, this.b[1] + 5);
-        line(this.a[0] - 5, this.a[1] - 5, this.b[0] - 5, this.b[1] - 5);
+        if (this.slope < 0) {
+            line(this.a[0] -5, this.a[1] + 5, this.b[0] - 5, this.b[1] + 5);
+            line(this.a[0] + 5, this.a[1] - 5, this.b[0] + 5, this.b[1] - 5);
+        } else {
+            line(this.a[0] + 5, this.a[1] + 5, this.b[0] + 5, this.b[1] + 5);
+            line(this.a[0] - 5, this.a[1] - 5, this.b[0] - 5, this.b[1] - 5);
+        }
+    }
+
+    lineSlope() {
+        // a[1] > b[1], as y decreases in cartesean coordinates, it increases in p5.js
+        var delta_x = this.b[0] - this.a[0];
+        var delta_y = this.a[1] - this.b[1];
+        if (delta_x == 0 || delta_y == 0) {
+            var m = 0;
+        } else {
+            var m = delta_y / delta_x;
+        }
+        return m;
+    }
+}
+
+// wind settings to affect the bubbles
+class windSettings {
+    constructor() {
+        this.value = false;
+        this.a = NaN;
+        this.b = NaN;
+        this.heading = NaN;
+        this.strength = 20;
+    }
+
+    getValue() {
+        return this.value;
+    }
+
+    setValue(value) {
+        this.value = value;
+    }
+
+    setCoordinates(point_a, point_b) {
+        this.a = point_a;
+        this.b = point_b;
+        var x = this.b[0] - this.a[0];
+        var y = this.a[1] - this.b[1];
+        this.heading = degrees(createVector(x, y).heading());
+    }
+
+    getCoordinates() {
+        return [this.heading, this.strength];
+    }
+}
+
+// how the bubbles get affected by the wind
+class windGrid {
+    constructor(width, height, resolution) {
+        this.width = width;
+        this.height = height;
+        this.resolution = resolution;
+        this.rows = Math.floor(this.width / this.resolution);
+        this.cols = Math.floor(this.height / this.resolution);
+        this.grid = new Array(this.rows);
+        for (var i = 0; i < this.rows; i++) {
+            this.grid[i] = new Array(this.cols);
+            for (var j = 0; j < this.cols; j++) {
+                this.grid[i][j] = new windSettings();
+            }
+        }
+    }
+
+    draw() {
+        // for (var i = 0; i < this.rows; i++) {
+        //     for (var j = 0; j < this.cols; j++) {
+        //         noFill();
+        //         if (this.grid[i][j].getValue()) {
+        //             fill('blue');
+        //         }
+        //         rect(i * this.resolution, j * this.resolution, this.resolution, this.resolution);
+        //     }
+        // }
+    }
+
+    getIndex(mouseX, mouseY) {
+        var i = Math.floor(mouseX / this.resolution);
+        var j = Math.floor(mouseY / this.resolution);
+        return [i, j];
+    }
+
+    setWind(mouseX, mouseY, point_a, point_b) {
+        var [i, j] = this.getIndex(mouseX, mouseY);
+        this.grid[i][j].setValue(true);
+        this.grid[i][j].setCoordinates(point_a, point_b);
+    }
+
+    checkWind(coordinates) {
+        var [i, j] = this.getIndex(coordinates[0], coordinates[1]);
+        if (this.grid[i][j].getValue()) {
+            return [true, i, j];
+        } return [false, NaN, NaN];
+    }
+
+    windParameters(i, j) {
+        return this.grid[i][j].getCoordinates();
     }
 }
